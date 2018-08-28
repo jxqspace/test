18e01c98a5d6b05b79dda2ee20b18a0826d6ae2d
jxqspace <42763209+jxqspace@users.noreply.github.com>
2018-08-28 15:15:08 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,6 @@
+# Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+# Type "copyright", "credits" or "license()" for more information.
+# >>>
+# >>> print "hello world"
+# SyntaxError: Missing parentheses in call to 'print'. Did you mean print("hello world")?
+# >>> print "hello world"
